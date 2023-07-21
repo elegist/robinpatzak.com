168b8188ea2036f76226255d8f1c8d646c0487df
Robin Patzak <robin.patzak@proton.me>
2023-07-22 00:16:35 +0200

    add typewriter animation to hero
    added a typewriter animation listing out all the expertises writing it
    into a span in the html document. variables for animation durations are
    very modular and can be easily edited.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,45 @@
-console.log("Robin Patzak - Developer");
+const variableHeroTextElement = document.getElementById("variable-hero-text");
+const blinkingCursorElement = document.getElementById("blinking-cursor");
+
+const heroTexts = ["Frontend", "Backend", "Games", "Mobile Apps"];
+
+let index = 0;
+let isDeleting = false;
+let text = "";
+
+const typingDuration = 150;
+const deletingDuration = typingDuration / 2;
+
+const blinkingDuration = 600;
+const waitDuration = 1600;
+
+function typewriter() {
+  const currentText = heroTexts[index];
+  if (isDeleting) {
+    text = currentText.substring(0, text.length - 1);
+  } else {
+    text = currentText.substring(0, text.length + 1);
+  }
+  variableHeroTextElement.textContent = text;
+
+  let timeDelay = isDeleting ? deletingDuration : typingDuration;
+
+  if (!isDeleting && text === currentText) {
+    isDeleting = true;
+    timeDelay += waitDuration;
+  } else if (isDeleting && text === "") {
+    isDeleting = false;
+    timeDelay += waitDuration / 2;
+    index = (index + 1) % heroTexts.length;
+  }
+
+  setTimeout(typewriter, timeDelay);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typewriter();
+  setInterval(() => {
+    blinkingCursorElement.style.opacity =
+      blinkingCursorElement.style.opacity === "0" ? "1" : "0";
+  }, blinkingDuration);
+});
